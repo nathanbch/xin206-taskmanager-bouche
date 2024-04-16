f9bd819fafbc8656a7829ce0914537db5fbd2a28
II-PTM-II <113678447+II-PTM-II@users.noreply.github.com>
2024-04-16 17:14:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fonctionnalite import *
 
-def afficher_menu():
+def afficher_menu(taches):
+    afficher_taches(taches)
     print("\nMenu:")
     print("1. Ajouter une tâche")
     print("2. Supprimer une tâche")
     print("3. Marquer une tâche comme terminée")
-    print("4. Afficher les tâches")
-    print("5. Quitter")
+    print("4. Quitter")
 
 if __name__ == "__main__":
     taches = charger_taches()
 
     while True:
-        afficher_menu()
+        afficher_menu(taches)
         choix = input("Entrez votre choix : ")
 
         if choix == "1":
@@ -22,8 +22,6 @@ if __name__ == "__main__":
         elif choix == "3":
             marquer_comme_terminee(taches)
         elif choix == "4":
-            afficher_taches(taches)
-        elif choix == "5":
             sauvegarder_taches(taches)
             print("Sortie du gestionnaire de tâches.")
             break
